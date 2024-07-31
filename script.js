aeba1ee5d2e9e59da199b763a78a06c029b42fa8
7Umair-Ahmed7 <7umairahmed7@gmail.com>
2024-07-31 22:41:04 +0500

    Search functionality

--- script.js
+++ script.js
@@ -1,54 +1,64 @@
 let searchInput = document.querySelector(".search");
 let searchBtn = document.querySelector(".search-btn");
 let temperatureElement = document.querySelector(".temperature").firstElementChild;
-let imgWeather = document.getElementsByClassName("img-weather")[0]
+let imgWeather = document.getElementsByClassName("img-weather")[0];
 let cityElement = document.querySelector(".city");
 let humidityElement = document.querySelector(".humidity-val-element");
 let windElement = document.querySelector(".wind-val-element");
 
 
-let city = "new york";
-let URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=99c975fda5cd1cffbf5e521a6aa3a34e`;
 
-async function getData() {
+
+async function getData(city = "manchester") {
     try {
+        let URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=99c975fda5cd1cffbf5e521a6aa3a34e`;
         let a = await fetch(URL);
         let response = await a.json();
         temperatureElement.innerHTML = response.main.temp;
         cityElement.innerHTML = city;
         windElement.innerHTML = response.wind.speed + " km/h";
         humidityElement.innerHTML = response.main.humidity + "%";
-        if(response.weather[0].main == "Clouds"){
+        if (response.weather[0].main == "Clouds") {
             imgWeather.src = "images/clouds.png";
-          
+
         }
-        else if(response.weather[0].main == "Clear"){
+        else if (response.weather[0].main == "Clear") {
             imgWeather.src = "images/clear.png";
-            
+
         }
-        else if(response.weather[0].main == "Rain"){
+        else if (response.weather[0].main == "Rain") {
             imgWeather.src = "images/rain.png";
-            
+
         }
-        else if(response.weather[0].main == "Drizzle"){
+        else if (response.weather[0].main == "Drizzle") {
             imgWeather.src = "images/drizzle.png";
-            
+
         }
-        else if(response.weather[0].main == "Snow"){
+        else if (response.weather[0].main == "Snow") {
             imgWeather.src = "images/snow.png";
-            
+
         }
-        else{
+        else {
             imgWeather.src = "images/clear.png";
-           
+
         }
 
-        console.log(response.weather[0].main);
     } catch (error) {
         console.log(error);
     }
 
 }
+getData("manchester");
 
-getData();
+searchBtn.addEventListener("click", () => {
+    let searchValue = searchInput.value;
+    if (searchValue != "") {
+        getData(searchValue);
+    }
+
+
+})
+
+
+
 
