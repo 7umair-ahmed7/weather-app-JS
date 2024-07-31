a67deca49cc309f58abe0a7bfce127d41933a634
7Umair-Ahmed7 <7umairahmed7@gmail.com>
2024-07-31 22:21:39 +0500

    interaction added

---
+++ script.js
@@ -0,0 +1,54 @@
+let searchInput = document.querySelector(".search");
+let searchBtn = document.querySelector(".search-btn");
+let temperatureElement = document.querySelector(".temperature").firstElementChild;
+let imgWeather = document.getElementsByClassName("img-weather")[0]
+let cityElement = document.querySelector(".city");
+let humidityElement = document.querySelector(".humidity-val-element");
+let windElement = document.querySelector(".wind-val-element");
+
+
+let city = "new york";
+let URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=99c975fda5cd1cffbf5e521a6aa3a34e`;
+
+async function getData() {
+    try {
+        let a = await fetch(URL);
+        let response = await a.json();
+        temperatureElement.innerHTML = response.main.temp;
+        cityElement.innerHTML = city;
+        windElement.innerHTML = response.wind.speed + " km/h";
+        humidityElement.innerHTML = response.main.humidity + "%";
+        if(response.weather[0].main == "Clouds"){
+            imgWeather.src = "images/clouds.png";
+          
+        }
+        else if(response.weather[0].main == "Clear"){
+            imgWeather.src = "images/clear.png";
+            
+        }
+        else if(response.weather[0].main == "Rain"){
+            imgWeather.src = "images/rain.png";
+            
+        }
+        else if(response.weather[0].main == "Drizzle"){
+            imgWeather.src = "images/drizzle.png";
+            
+        }
+        else if(response.weather[0].main == "Snow"){
+            imgWeather.src = "images/snow.png";
+            
+        }
+        else{
+            imgWeather.src = "images/clear.png";
+           
+        }
+
+        console.log(response.weather[0].main);
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+getData();
+
